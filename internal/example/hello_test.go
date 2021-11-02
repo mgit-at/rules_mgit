8739850f785f3e3f8bc787524f917c9649de0d52
Sabine Kozeschnik <skozeschnik@mgit.at>
2021-11-02 12:19:17 +0100

    fix bazel BUILD

--- internal/example/hello_test.go
+++ internal/example/hello_test.go
@@ -4,11 +4,13 @@ import (
 	"bytes"
 	"strings"
 	"testing"
+
+	"mgit.at/rules_go/internal/example/lib"
 )
 
 func TestHello(t *testing.T) {
 	buf := &bytes.Buffer{}
-	printHello(buf)
+	lib.PrintHello(buf)
 	if want, got := "Hello rules_mgIT!\n", buf.String(); !strings.HasPrefix(got, want) {
 		t.Errorf("expected prefix %q, got %q", want, got)
 	}
